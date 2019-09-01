98a30e8234b92ab97f8994a92fa49a8d87ca2cac
alex <alexvatti@gmail.com>
2019-09-01 19:25:18 +0530

    check for binary search tree code, is updated

--- Data-Structures/is-binary-search-tree.c
+++ Data-Structures/is-binary-search-tree.c
@@ -19,3 +19,46 @@ bool checkBST(Node* root) {
     } 
     return true; 
 }
+
+int  maxValue(Node * root)
+{
+    if( !root ) 
+	return root->data;
+    while( root->right ){
+        root = root->right;
+    }
+    return root->data;
+}
+ 
+int  minValue(Node * root){
+    if( !root ) 
+	return root->data;
+    while( root->left ){
+        root = root->left;
+    }
+    return root->data;
+}
+
+
+/* Returns true if a binary tree is a binary search tree */ 
+int isBST(struct node* node)  
+{  
+  if (node == NULL)  
+    return(true);  
+      
+  /* false if the max of the left is > than us */
+  if (node->left!=NULL && maxValue(node->left) > node->data)  
+    return(false);  
+      
+  /* false if the min of the right is <= than us */
+  if (node->right!=NULL && minValue(node->right) < node->data)  
+    return(false);  
+    
+  /* false if, recursively, the left or right is not a BST */
+  if (!isBST(node->left) || !isBST(node->right))  
+    return(false);  
+      
+  /* passing all that, it's a BST */
+  return(true);  
+}  
+
